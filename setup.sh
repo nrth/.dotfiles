152b926edae23df9b395c8079097e9bebbf4a23f
Attila Földes <foeldes@ionauts.com>
2015-05-13 01:46:36 +0200

    added setup script

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# little script to install dotfiles
+
+dir=~/dotfiles
+backup=~/dotfiles_backup
+files="bashrc vimrc gitconfig atom/config.cson"
+
+run()
+{
+    cd $dir
+    echo "Setting up dotfiles from" $dir
+
+    for file in $files; do
+
+        # unlink existent symlink
+        if [ -h ~/.$file ]; then
+            echo "Unlinking" $file
+            unlink ~/.$file
+        fi
+
+        # file is not symlink, backup dotfiles
+        if [ -f ~/.$file ]; then
+
+            # create backup directory if necessary
+            if [ ! -e $backup ]; then
+                echo "Creating backup directory at" $backup
+                mkdir $backup
+            fi
+
+            # backup directory is a file
+            if [ ! -d $backup ]; then
+                echo "ERROR: $backup not an directory"
+                exit 0
+            fi
+
+            echo "Saving backup for" $file
+            mv ~/.$file $backup/
+        fi
+
+        # symlink files from dotfiles repo
+        echo "Creating symlink for" $file
+        ln -s $dir/$file ~/.$file
+
+    done
+
+    echo "Done :)"
+}
+
+run
+
